1150cb5e5709bccbfc215402610d8149a49e339d
Christos Lytras <christos.lytras@gmail.com>
2021-01-22 03:12:06 +0200

    build(release): add make release script

---
+++ scripts/makeRelease.js
@@ -0,0 +1,131 @@
+/* eslint-disable prettier/prettier */
+const del = require('del');
+const ora = require('ora');
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob-promise');
+const copy = require('recursive-copy');
+const cpy = require('cpy');
+
+const distPath = './dist';
+const releasePath = './release';
+const sourcePath = './src';
+
+(async () => {
+  (await deleteReleaseDir()) &&
+  (await copyDistFiles()) &&
+  (await fixSourceMapPaths()) &&
+  (await copySourceFiles()) &&
+  (await copyPackageFiles());
+})();
+
+async function deleteReleaseDir() {
+  const deleteReleaseDirSpinner = ora('Deleting release directory');
+
+  try {
+    deleteReleaseDirSpinner.start();
+    const deleted = await del([`${releasePath}/**/*`, `!${releasePath}`]);
+    deleteReleaseDirSpinner.text += ` (deleted ${deleted.length} files/dirs)`;
+    deleteReleaseDirSpinner.succeed();
+    return true;
+  } catch (error) {
+    deleteReleaseDirSpinner.text = `${chalk.red(
+      'Fail to delete release directory'
+    )}: ${error.message}`;
+    deleteReleaseDirSpinner.fail();
+    return false;
+  }
+}
+
+async function copyDistFiles() {
+  const copyDistFilesSpinner = ora('Copying dist files for release');
+
+  try {
+    copyDistFilesSpinner.start();
+    await copy(distPath, releasePath);
+    copyDistFilesSpinner.succeed();
+    return true;
+  } catch (error) {
+    copyDistFilesSpinner.text = `${chalk.red(
+      'Fail to copy dist directory to release'
+    )}: ${error.message}`;
+    copyDistFilesSpinner.fail();
+    return false;
+  }
+}
+
+async function fixSourceMapPaths() {
+  const fixSourceMapsSpinner = ora('Fixing source maps paths');
+
+  try {
+    fixSourceMapsSpinner.start();
+    const sourceMaps = await glob(`${releasePath}/**/*.js.map`);
+    fixSourceMapsSpinner.succeed();
+
+    let fixedFiles = 0;
+
+    for (const file of sourceMaps) {
+      const json = fs.readFileSync(file, { encoding: 'utf-8' });
+      const data = JSON.parse(json);
+
+      if (data?.sources?.length > 0) {
+        data.sources = data.sources.map(source => source.replace(/.*\/src/, './src'));
+        fs.writeFileSync(file, JSON.stringify(data), { encoding: 'utf-8' });
+        fixedFiles++;
+      }
+    }
+
+    fixSourceMapsSpinner.text += ` (fixed ${fixedFiles} files)`;
+    fixSourceMapsSpinner.succeed();
+    return true;
+  } catch (error) {
+    fixSourceMapsSpinner.text = `${chalk.red(
+      'Fail to fix source maps paths'
+    )}: ${error.message}`;
+    fixSourceMapsSpinner.fail();
+    return false;
+  }
+}
+
+async function copySourceFiles() {
+  const copySrcFilesSpinner = ora('Copying source files for release');
+
+  try {
+    copySrcFilesSpinner.start();
+    const srcDest = `${releasePath}/src`;
+    fs.mkdirSync(srcDest);
+    await copy(sourcePath, srcDest);
+    copySrcFilesSpinner.succeed();
+    return true;
+  } catch (error) {
+    copySrcFilesSpinner.text = `${chalk.red(
+      'Fail to copy source files to release'
+    )}: ${error.message}`;
+    copySrcFilesSpinner.fail();
+    return false;
+  }
+}
+
+async function copyPackageFiles() {
+  const copyPkgFilesSpinner = ora('Copying package files for release');
+
+  try {
+    copyPkgFilesSpinner.start();
+
+    await cpy([
+      'package.json',
+      'LICENSE',
+      'README.md'
+    ], releasePath);
+
+    copyPkgFilesSpinner.succeed();
+    return true;
+  } catch (error) {
+    copyPkgFilesSpinner.text = `${chalk.red(
+      'Fail to copy package files to release'
+    )}: ${error.message}`;
+    copyPkgFilesSpinner.fail();
+    return false;
+  }
+}
